4c33fef90a5754268b5eb7c962d75578e7115357
Christoph Franke <krito@krito.de>
2018-04-15 16:06:42 +0200

    added imprint

---
+++ www/pages/imprint.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+    <header-slide :header="page.header" v-if="page.header" />
+		<breadcrumb title="Imprint" :path="path" />
+    <div v-html="page.text" class="container"></div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+	padding: 60px 0 200px 0;
+	max-width: 50vw;
+}
+</style>
+
+<script>
+import components from '~/components';
+import page from '~/apollo/queries/page.gql';
+
+export default {
+	components,
+  apollo: {
+  	page: {
+	    query: page,
+			prefetch: () => ({slug: 'imprint' }),
+      variables: () => ({slug: 'imprint' })
+  	}
+  },
+	data() {
+		return {
+  		path: [
+	  		{
+	  			slug: '/',
+	  			title: 'Home'
+	  		}
+	  	],
+	  	page: {
+	  		title: '...'
+	  	}
+		}
+	},
+	head() {
+		return {
+			title: this.page.title
+		}
+	}
+}
+</script>
