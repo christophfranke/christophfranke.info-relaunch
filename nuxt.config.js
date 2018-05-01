66613bd5dc20d7eaf81b5cbaedc885175b134419
Christoph Franke <krito@krito.de>
2018-05-01 15:03:24 +0200

    added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,4 +7,17 @@ module.exports = {
     }
   },
   loading: false,
+  head: {
+  	titleTemplate: 'Christoph Franke - %s',
+  	link: [
+  		{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  		{ rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  		{ rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  		{ rel: "manifest", href: "/site.webmanifest" }
+  	],
+  	meta: [
+  		{ name: "msapplication-TileColor", content: "#272727" },
+  		{ name: "theme-color", content: "#ffffff" }
+  	]
+  }
 }
